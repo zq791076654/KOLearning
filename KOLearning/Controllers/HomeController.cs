47c006ff518aeafdce9c6e8ca3042b15a1274e0f
qiangz2 <qiangz2@wicresoft.com>
2019-08-08 18:31:36 +0800

    add SimpleModel

--- KOLearning/Controllers/HomeController.cs
+++ KOLearning/Controllers/HomeController.cs
@@ -9,7 +9,57 @@ namespace KOLearning.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult FullName()
+        {
+            return View();
+        }
+
+        public ActionResult ClickCounter()
+        {
+            return View();
+        }
+
+        public ActionResult SimpleList()
+        {
+            return View();
+        }
+
+        public ActionResult BetterList()
+        {
+            return View();
+        }
+
+        public ActionResult ControlTypes()
+        {
+            return View();
+        }
+
+        public ActionResult WorkingWithCollections()
+        {
+            return View();
+        }
+
+        public ActionResult PagedGrid()
+        {
+            return View();
+        }
+
+        public ActionResult AnimatedTransitions()
+        {
+            return View();
+        }
+
+        public ActionResult ContactsEditor()
+        {
+            return View();
+        }
+
+        public ActionResult GridEditor()
+        {
+            return View();
+        }
+
+        public ActionResult ShoppingCart()
         {
             return View();
         }
