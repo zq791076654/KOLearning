2f73706f6ba9c3df12b47fd5cffa143b64dd6ef5
qiangz2 <qiangz2@wicresoft.com>
2019-08-12 19:00:39 +0800

    learning continue

---
+++ KOLearning/Controllers/BeginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KOLearning.Controllers
+{
+    public class BeginController : Controller
+    {
+        // GET: Begin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
